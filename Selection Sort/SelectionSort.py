71df5fc67fcc5ab66c325369557b1c6b8f0cbe40
Rishikesh <43941908+Rishikesh9919@users.noreply.github.com>
2020-10-16 13:27:25 +0530

    Update SelectionSort.py

--- Selection Sort/SelectionSort.py
+++ Selection Sort/SelectionSort.py
@@ -3,13 +3,16 @@ import random
 
 def selection_sort(array):
     for i in range(len(array) - 1):
+        # to keep track of position of minimum value
         min_val_pos = i
         for j in range(i, len(array)):
             if arr[j] < arr[min_val_pos]:
                 min_val_pos = j
+        # Swapping with the minimum number from the list
         array[i], array[min_val_pos] = array[min_val_pos], array[i]
 
 
+# for generating 15 random numbers to sort 
 arr = [random.randint(1, 99) for i in range(15)]
 print('Before sorting: ', arr)
 selection_sort(arr)
